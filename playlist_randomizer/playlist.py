fb0aff8e3fb6cd68ad78832f05b4e69eac1d7cab
adradan <ericvsolcruz@gmail.com>
2020-08-05 22:51:17 -0400

    Added creating playlist for user

--- playlist_randomizer/playlist.py
+++ playlist_randomizer/playlist.py
@@ -3,13 +3,26 @@ from playlist_randomizer.songs import Songs
 
 class Playlist:
     def __init__(self):
-        self.df = Songs().start_init_search()
+        self.playlist = None
+        self.song = Songs()
+        self.df = self.song.start_init_search()
+        self.sp = self.song.sp
+        self.user = self.sp.current_user()
+        # self.user_name = self.user['display_name']
+        # self.user_id = self.user['id']
         print(self.df)
+        self.create_playlist()
 
-    def song_list(self):
-        pass
-
-
+    def create_playlist(self):
+        user_name = self.user['display_name']
+        user_id = self.user['id']
+        playlist_name = f'This is {user_name}'
+        description = f'Compilation of {user_name}\'s favorite artists.'
+        context = {'user': user_id,
+                   'name': playlist_name,
+                   'public': True,
+                   'description': description}
+        self.playlist = self.sp.user_playlist_create(**context)
 
 
 Playlist()
